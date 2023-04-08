34e551ab2bab2482d6d423e237be4c8e459cc5df
Fahamin <fahamin00@gmail.com>
2023-04-08 11:21:35 +0600

    v3

--- app/src/main/java/medication/smartpatient/subcriptionv5/RemoveAdsActivity.kt
+++ app/src/main/java/medication/smartpatient/subcriptionv5/RemoveAdsActivity.kt
@@ -1,280 +1,239 @@
-package medication.smartpatient.subcriptionv5;
+package medication.smartpatient.subcriptionv5
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.FrameLayout
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.billingclient.api.*
+import com.android.billingclient.api.BillingFlowParams.ProductDetailsParams
+import com.android.billingclient.api.QueryProductDetailsParams.Product
+import com.google.android.material.snackbar.Snackbar
+import com.google.common.collect.ImmutableList
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.ProgressBar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.android.billingclient.api.AcknowledgePurchaseParams;
-import com.android.billingclient.api.BillingClient;
-import com.android.billingclient.api.BillingClientStateListener;
-import com.android.billingclient.api.BillingFlowParams;
-import com.android.billingclient.api.BillingResult;
-import com.android.billingclient.api.ProductDetails;
-import com.android.billingclient.api.Purchase;
-import com.android.billingclient.api.QueryProductDetailsParams;
-import com.android.billingclient.api.QueryPurchasesParams;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.common.collect.ImmutableList;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class RemoveAdsActivity extends AppCompatActivity implements ItemClickListener {
-
-    Activity activity;
-    Prefs prefs;
-    private BillingClient billingClient;
-    List<ProductDetails> productDetailsList;
-    ProgressBar loadProducts;
-    RecyclerView recyclerView;
-    Handler handler;
-
-    private FrameLayout adContainerView;
+class RemoveAdsActivity : AppCompatActivity(), ItemClickListener {
+    var activity: Activity? = null
+    var prefs: Prefs? = null
+    private var billingClient: BillingClient? = null
+    var productDetailsList: MutableList<ProductDetails>? = null
+    var loadProducts: ProgressBar? = null
+    var recyclerView: RecyclerView? = null
+    var handler: Handler? = null
+    private val adContainerView: FrameLayout? = null
 
     @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.activity_remove_ads);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        initViews();
-        setTitle("Remove Ads");
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_remove_ads)
+        val actionBar = supportActionBar
+        actionBar?.hide()
+        initViews()
+        title = "Remove Ads"
         //Initialize a BillingClient with PurchasesUpdatedListener onCreate method
         billingClient = BillingClient.newBuilder(this)
-                .enablePendingPurchases()
-                .setListener(
-                        (billingResult, list) -> {
-                            if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && list != null) {
-                                for (Purchase purchase : list) {
-                                    handlePurchase(purchase);
-                                }
-                            }
-                        }
-                ).build();
+            .enablePendingPurchases()
+            .setListener { billingResult: BillingResult, list: List<Purchase>? ->
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && list != null) {
+                    for (purchase in list) {
+                        handlePurchase(purchase)
+                    }
+                }
+            }.build()
 
         //start the connection after initializing the billing client
-        establishConnection();
-    /*    //restore purchases
+        establishConnection()
+        /*    //restore purchases
         btn_restore_fab.setOnClickListener(v -> {
             restorePurchases();
         });*/
 
         //Checks if the user has a removeAd if not then show ads.
         //Checks if the user has a premium/subscription if not then show ads.
-        if (prefs.getPremium() == 0) {
-            if (!prefs.isRemoveAd()) {
-                Log.d("RemoveAds", "Remove ads off");
+        if (prefs!!.premium == 0) {
+            if (!prefs!!.isRemoveAd) {
+                Log.d("RemoveAds", "Remove ads off")
             } else {
-                Log.d("RemoveAds", "Remove ads On");
+                Log.d("RemoveAds", "Remove ads On")
                 //    mAdView.setVisibility(View.GONE);
             }
         }
-
     }
 
-    void establishConnection() {
-
-        billingClient.startConnection(new BillingClientStateListener() {
-            @Override
-            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
-                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+    fun establishConnection() {
+        billingClient!!.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
                     // The BillingClient is ready. You can query purchases here.
-                    showProducts();
+                    showProducts()
                 }
             }
 
-            @Override
-            public void onBillingServiceDisconnected() {
+            override fun onBillingServiceDisconnected() {
                 // Try to restart the connection on the next request to
                 // Google Play by calling the startConnection() method.
-                establishConnection();
+                establishConnection()
             }
-        });
+        })
     }
 
     @SuppressLint("SetTextI18n")
-    void showProducts() {
-        ImmutableList productList = ImmutableList.of(
-                //Product 1 = index is 0
+    fun showProducts() {
+        val productList = ArrayList<QueryProductDetailsParams.Product>()
+        productList.add(
+            QueryProductDetailsParams.Product.newBuilder()
+                .setProductId("product_id_example")
+                .setProductType(BillingClient.ProductType.SUBS)
+                .build(),
 
-                //Product 2 = index is 1
 
+            )
 
-                QueryProductDetailsParams.Product.newBuilder()
-                        .setProductId("ks2")
-                        .setProductType(BillingClient.ProductType.SUBS)
-                        .build(),
-
-                QueryProductDetailsParams.Product.newBuilder()
-                        .setProductId("ks3")
-                        .setProductType(BillingClient.ProductType.SUBS)
-                        .build(),
-
-                QueryProductDetailsParams.Product.newBuilder()
-                        .setProductId("ksw")
-                        .setProductType(BillingClient.ProductType.SUBS)
-                        .build()
-        );
-
-        QueryProductDetailsParams params = QueryProductDetailsParams.newBuilder()
-                .setProductList(productList)
-                .build();
-
-        billingClient.queryProductDetailsAsync(
-                params,
-                (billingResult, prodDetailsList) -> {
-                    // Process the result
-                    productDetailsList.clear();
-                    handler.postDelayed(() -> {
-                        loadProducts.setVisibility(View.INVISIBLE);
-                        productDetailsList.addAll(prodDetailsList);
-                        RemoveAdsAdapter adapter = new RemoveAdsAdapter(getApplicationContext(), productDetailsList, RemoveAdsActivity.this, this);
-                        recyclerView.setHasFixedSize(true);
-                        recyclerView.setLayoutManager(new LinearLayoutManager(RemoveAdsActivity.this, LinearLayoutManager.VERTICAL, false));
-                        recyclerView.setAdapter(adapter);
-                    }, 2000);
-
-                }
-        );
-
-    }
-
-    void launchPurchaseFlow(ProductDetails productDetails) {
-        assert productDetails.getSubscriptionOfferDetails() != null;
-        ImmutableList<BillingFlowParams.ProductDetailsParams> productDetailsParamsList =
-                ImmutableList.of(
-                        BillingFlowParams.ProductDetailsParams.newBuilder()
-                                .setProductDetails(productDetails)
-                                .setOfferToken(productDetails.getSubscriptionOfferDetails().get(0).getOfferToken())
-                                .build()
-                );
-        BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
-                .setProductDetailsParamsList(productDetailsParamsList)
-                .build();
-
-        billingClient.launchBillingFlow(activity, billingFlowParams);
-    }
-
-    void handlePurchase(Purchase purchases) {
-        if (!purchases.isAcknowledged()) {
-            billingClient.acknowledgePurchase(AcknowledgePurchaseParams
-                    .newBuilder()
-                    .setPurchaseToken(purchases.getPurchaseToken())
-                    .build(), billingResult -> {
-
-                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
-                    //Setting setIsRemoveAd to true
-                    // true - No ads
-                    // false - showing ads.
-                    prefs.setRemoveAd(true);
-                    reloadScreen();
-                }
-            });
+        val params = QueryProductDetailsParams.newBuilder()
+            .setProductList(productList)
+            .build()
+        billingClient!!.queryProductDetailsAsync(
+            params
+        ) { billingResult: BillingResult?, prodDetailsList: List<ProductDetails>? ->
+            // Process the result
+            productDetailsList!!.clear()
+            handler!!.postDelayed({
+                loadProducts!!.visibility = View.INVISIBLE
+                productDetailsList!!.addAll(prodDetailsList!!)
+                val adapter = RemoveAdsAdapter(
+                    applicationContext,
+                    productDetailsList!!,
+                    this@RemoveAdsActivity,
+                    this
+                )
+                recyclerView!!.setHasFixedSize(true)
+                recyclerView!!.layoutManager =
+                    LinearLayoutManager(this@RemoveAdsActivity, LinearLayoutManager.VERTICAL, false)
+                recyclerView!!.adapter = adapter
+            }, 2000)
         }
     }
 
-    private void reloadScreen() {
-        //Reload the screen to activate the removeAd and remove the actual Ad off the screen.
-        finish();
-        overridePendingTransition(0, 0);
-        startActivity(getIntent());
-        overridePendingTransition(0, 0);
+    fun launchPurchaseFlow(productDetails: ProductDetails) {
+        assert(productDetails.subscriptionOfferDetails != null)
+        val productDetailsParamsList = ImmutableList.of(
+            ProductDetailsParams.newBuilder()
+                .setProductDetails(productDetails)
+                .setOfferToken(productDetails.subscriptionOfferDetails!![0].offerToken)
+                .build()
+        )
+        val billingFlowParams = BillingFlowParams.newBuilder()
+            .setProductDetailsParamsList(productDetailsParamsList)
+            .build()
+        billingClient!!.launchBillingFlow(activity!!, billingFlowParams)
     }
 
-    protected void onResume() {
-        super.onResume();
-        billingClient.queryPurchasesAsync(
-                QueryPurchasesParams.newBuilder().setProductType(BillingClient.ProductType.INAPP).build(),
-                (billingResult, list) -> {
-                    if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
-                        for (Purchase purchase : list) {
-                            if (purchase.getPurchaseState() == Purchase.PurchaseState.PURCHASED && !purchase.isAcknowledged()) {
-                                handlePurchase(purchase);
+    fun handlePurchase(purchases: Purchase) {
+        if (!purchases.isAcknowledged) {
+            billingClient!!.acknowledgePurchase(
+                AcknowledgePurchaseParams
+                    .newBuilder()
+                    .setPurchaseToken(purchases.purchaseToken)
+                    .build()
+            ) { billingResult: BillingResult ->
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    //Setting setIsRemoveAd to true
+                    // true - No ads
+                    // false - showing ads.
+                    prefs!!.isRemoveAd = true
+                    reloadScreen()
+                }
+            }
+        }
+    }
+
+    private fun reloadScreen() {
+        //Reload the screen to activate the removeAd and remove the actual Ad off the screen.
+        finish()
+        overridePendingTransition(0, 0)
+        startActivity(intent)
+        overridePendingTransition(0, 0)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        billingClient!!.queryPurchasesAsync(
+            QueryPurchasesParams.newBuilder().setProductType(BillingClient.ProductType.INAPP)
+                .build()
+        ) { billingResult: BillingResult, list: List<Purchase> ->
+            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                for (purchase in list) {
+                    if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED && !purchase.isAcknowledged) {
+                        handlePurchase(purchase)
+                    }
+                }
+            }
+        }
+    }
+
+    fun restorePurchases() {
+        billingClient = BillingClient.newBuilder(this).enablePendingPurchases()
+            .setListener { billingResult: BillingResult?, list: List<Purchase?>? -> }
+            .build()
+        val finalBillingClient = billingClient!!
+        billingClient!!.startConnection(object : BillingClientStateListener {
+            override fun onBillingServiceDisconnected() {
+                establishConnection()
+            }
+
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    finalBillingClient.queryPurchasesAsync(
+                        QueryPurchasesParams.newBuilder()
+                            .setProductType(BillingClient.ProductType.INAPP).build()
+                    ) { billingResult1: BillingResult, list: List<Purchase?> ->
+                        if (billingResult1.responseCode == BillingClient.BillingResponseCode.OK) {
+                            if (list.size > 0) {
+                                prefs!!.isRemoveAd = true // set true to activate remove ad feature
+                            } else {
+                                prefs!!.isRemoveAd =
+                                    false // set false to de-activate remove ad feature
                             }
                         }
                     }
                 }
-        );
-    }
-
-    void restorePurchases() {
-        billingClient = BillingClient.newBuilder(this).enablePendingPurchases().setListener((billingResult, list) -> {
-        }).build();
-        final BillingClient finalBillingClient = billingClient;
-        billingClient.startConnection(new BillingClientStateListener() {
-            @Override
-            public void onBillingServiceDisconnected() {
-                establishConnection();
             }
-
-            @Override
-            public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
-                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
-                    finalBillingClient.queryPurchasesAsync(
-                            QueryPurchasesParams.newBuilder().setProductType(BillingClient.ProductType.INAPP).build(), (billingResult1, list) -> {
-                                if (billingResult1.getResponseCode() == BillingClient.BillingResponseCode.OK) {
-                                    if (list.size() > 0) {
-                                        prefs.setRemoveAd(true); // set true to activate remove ad feature
-                                    } else {
-                                        prefs.setRemoveAd(false); // set false to de-activate remove ad feature
-                                    }
-                                }
-                            });
-                }
-            }
-        });
+        })
     }
 
-    private void initViews() {
-
-        activity = this;
-        handler = new Handler();
-        prefs = new Prefs(this);
-        productDetailsList = new ArrayList<>();
-        recyclerView = findViewById(R.id.recycleViewID);
-        loadProducts = findViewById(R.id.progressID);
-
+    private fun initViews() {
+        activity = this
+        handler = Handler()
+        prefs = Prefs(this)
+        productDetailsList = ArrayList()
+        recyclerView = findViewById(R.id.recycleViewID)
+        loadProducts = findViewById(R.id.progressID)
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+    override fun onBackPressed() {
+        super.onBackPressed()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
-    public void showSnackbar(View view, String message) {
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show();
+    fun showSnackbar(view: View?, message: String?) {
+        Snackbar.make(view!!, message!!, Snackbar.LENGTH_SHORT).show()
     }
 
-    @Override
-    public void clickListener(int pos) {
-        launchPurchaseFlow(productDetailsList.get(pos));
-
+    override fun clickListener(pos: Int) {
+        launchPurchaseFlow(productDetailsList!![pos])
     }
 }
